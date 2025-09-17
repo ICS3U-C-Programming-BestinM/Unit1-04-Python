f96db048b76912f5cadab573c827d57d82d50a7b
Bestin Binu Mathew <bestin.mathew@stu.ocsb.ca>
2025-09-17 18:29:23 +0000

    update

---
+++ basic_math.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# Created By: Your Name Bestin Mathew
+# Date: Month Day, Year 10:16:24
+# Write what this program does.
+# This program performs basic math operations and prints the results.
+
+
+def main():
+    print("7-3={}".format(7 - 3))
+    print("2+4={}".format(2 + 4))
+    print("8*6={}".format(8 * 6))
+    print("4/2={}".format(4 / 2))
+    print("1/2={}".format(1 / 2))
+    print("2+5*3={}".format(2 + 5 * 3))
+    print("2^3={}".format(2 ^ 3))
+    print("2**3={}".format(2**3))
+    print("16**0.5={}".format(16**0.5))
+
+
+if __name__ == "__main__":
+    main()
